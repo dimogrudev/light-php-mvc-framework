007299bcb8048a49131a4af85cbec8a938d05a58
Vadim Grudev <345frosty@gmail.com>
2023-09-19 20:40:47 -0300

    Update Model.php

--- Core/Model.php
+++ Core/Model.php
@@ -2,6 +2,14 @@
 
 namespace Core;
 
+use Core\Modules\Database;
+
 abstract class Model
 {
+    protected Database $db;
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance();
+    }
 }
